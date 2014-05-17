c0742b8879b0af8ab99d570e2e4beaeb64156e4c
Torrent Glenn <torrentglenn@Torrent-Glenns-MacBook-Pro.local>
2014-05-17 16:19:18 -0400

    update

--- darking.py
+++ darking.py
@@ -82,44 +82,19 @@ class controlPoint(blobPoint):
 		self.kind = "controlPoint" 
 		#DO THIS GODDAM THING!!!!
 	def accelerate(self):
-		#	self.position = self.position.add(self.velocity)
-			
-			for point in self.parts:
-				"""	dist = math.sqrt(math.pow(self.position.x-point.position.x,2)+math.pow(self.position.y-point.position.y,2))
-				vect = physics.vector2d(self.position.x-point.position.x,self.position.y-point.position.y).unitize()
-				vect = vect.multiply(650)
-				vect = vect.divide(math.pow(dist,1.1))
-				"""
-			#	physics.applyForce(point,vect)	
-				
-			#	if self.rect.colliderect(point.rect):
-			#		self.velocity = physics.vector2d(0,0)
-			#		point.velocity = physics.vector2d(0,0)
-
-				
-				#if dist <= 50:
-				#	point.velocity = point.velocity.multiply(.8)
-				"""
-				if dist >= 200:
-					a = random.randint(-70,70)
-					b = random.randint(-70,70)
-					if abs(a) > 30:
-						a = random.randint(-70,70)
-					if abs(b) > 30:
-						b = random.randint(-70,70)
-					point.position = self.position.add(physics.vector2d(a,b))
-					"""			
 			for point in self.parts:#.controls:
 				
 				dist = math.sqrt(math.pow(self.position.x-point.position.x,2)+math.pow(self.position.y-point.position.y,2))
 				vect = physics.vector2d(self.position.x-point.position.x,self.position.y-point.position.y).unitize()
 				newdist = math.sqrt(math.pow(self.controls[0].position.x-point.position.x,2)+math.pow(self.controls[0].position.y-point.position.y,2))
 				
-				if dist > 20 and newdist > 20  and point.kind == "blobPoint" : #and newdist >  50:
+				if dist > 14 and newdist > 14  and point.kind == "blobPoint" : #and newdist >  50:
 					#point.velocity = point.velocity.multiply(.5)
-					#vect = vect.divide(math.pow(dist,1.001))
-
-					physics.applyForce(point,vect.multiply(3))
+					if dist + newdist > 100:
+						vect = vect.divide(math.pow(dist,1.001))
+						physics.applyForce(point,vect.multiply(30))
+					else:
+						physics.applyForce(point,vect.multiply(3))
 					#point.velocity = vect.multiply(5)
 					
 					#point.position = self.position
@@ -130,7 +105,7 @@ class controlPoint(blobPoint):
 					#physics.applyForce(point,vect)
 				if dist < 2 and point.kind == "blobPoint" :#or newdist < 40:
 					#point.velocity = vect.unitize().multiply(-10)
-					physics.applyForce(point,vect.reverse().multiply(5))
+					physics.applyForce(point,vect.reverse().multiply(3))
 					#point.velocity = vect.multiply(-4)
 						
 					
