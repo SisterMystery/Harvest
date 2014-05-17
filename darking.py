06e1c33b1db642cdbff0fb9b0805b262c9a6c07f
Torrent Glenn <torrentglenn@Torrent-Glenns-MacBook-Pro.local>
2014-05-17 16:09:53 -0400

    update

--- darking.py
+++ darking.py
@@ -3,31 +3,54 @@ from harvest_screen import *
 
 class blobPoint(object):
 	def __init__(self,x,y):
+		self.kind = "blobPoint"
 		# add itself to the Things-with-mass list? 
 		#get black pixel image and its 1x1 rectangle
-		self.image = pygame.image.load("images/blackPixel.bmp")
-		self.rect = self.image.get_rect().inflate(3,3)
+		self.image = pygame.image.load("images/blackPixelg.bmp")
+		self.rect = self.image.get_rect().inflate(5,5)
 		self.position = physics.vector2d(x,y)
 		self.velocity = physics.vector2d(0,0) #x,y velocities (pixels per tick)
 		#self.acceleration = physics.vector2d(0,0)  # x,y accelerations
 	 	self.temperature = 20 # maybe irrelevant? maybe super important? 
-		self.controlpt = False	
 
 
+	def bump(self,ptlist):
+		for point in ptlist:
+				 
+			if abs(self.position.x - point.position.x) < 1 and abs(self.position.y - point.position.y) < 1:
+				vect = physics.vector2d(self.position.x-point.position.x,self.position.y-point.position.y).unitize()
+				physics.applyForce(self,vect.multiply(1))
+				break
+	"""			
 	def bump(self,ptList):
-		i = self.rect.collidelist(ptList)
-		ptList[i].velocity,self.velocity = self.velocity.multiply(.6),ptList[i].velocity.multiply(.6)	
+		
+		bumps = self.rect.collidelistall(ptList)
+		
+		#if bumps:
+		#	self.velocity = self.velocity.multiply(.7).reverse()
 
+		
+		x = self.velocity.x
+		y = self.velocity.y
+		for i in bumps:
+			x += ptList[i].velocity.x
+			y += ptList[i].velocity.y
+			x /= len(ptList)
+			y /= len(ptList)
+			
+			self.velocity = physics.vector2d(x,y)
+	"""	
 	def accelerate(self):
 	# In this world there are very few forces: here we check and apply them all to this blobPoint
 		# accelerate for particle attraction
-		physics.applyForce(self,physics.Gravity)
+		#physics.applyForce(self,physics.Gravity)
 		# accelerate for solid?/liquid/air resistance
 		#if not self.controlpt: 
 		#	dragVect = self.velocity.multiply(-.2)
-		self.velocity = self.velocity.multiply(.95)
-		if self.velocity.x + self.velocity.y > 150:
-			self.velocity = physics.vector2d(10,10)
+		self.velocity = self.velocity.multiply(.95) #drag? 
+		if self.velocity.x + self.velocity.y > 350: #maximum speed?
+			one = self.velocity.unitize()
+			self.velocity = one.multiply(350)
 		 #accelerate for other forces?   			
 	def update(self):
 		if self.position.x > width:
@@ -42,7 +65,7 @@ class blobPoint(object):
 		if self.position.y < 0:
 			self.position.y = 1
 			self.velocity = self.velocity.reverse()	
-		self.velocity = self.velocity.multiply(.65)
+		
 		self.position = self.position.add(self.velocity)
 		self.accelerate()
 	
@@ -51,29 +74,33 @@ class blobPoint(object):
 		screen.blit(self.image,self.rect) 		
 		
 class controlPoint(blobPoint):
-	def __init__(self,x,y):
+	def __init__(self,x,y,minions):
 		blobPoint.__init__(self,x,y)
+		self.parts = minions
+		self.controls = []
+		self.image = pygame.image.load("images/control.bmp")
+		self.kind = "controlPoint" 
 		#DO THIS GODDAM THING!!!!
 	def accelerate(self):
+		#	self.position = self.position.add(self.velocity)
+			
 			for point in self.parts:
-				dist = math.sqrt(math.pow(self.position.x-point.position.x,2)+math.pow(self.position.y-point.position.y,2))
+				"""	dist = math.sqrt(math.pow(self.position.x-point.position.x,2)+math.pow(self.position.y-point.position.y,2))
 				vect = physics.vector2d(self.position.x-point.position.x,self.position.y-point.position.y).unitize()
-				vect = vect.multiply(350)
+				vect = vect.multiply(650)
+				vect = vect.divide(math.pow(dist,1.1))
+				"""
+			#	physics.applyForce(point,vect)	
 				
-				vect = vect.divide(math.pow(dist,1.5))
-				physics.applyForce(point,vect)	
-				if self.rect.colliderect(point.rect):
-					self.velocity = physics.vector2d(0,0)
-					point.velocity = physics.vector2d(0,0)
-				if dist <= 50 and point.controlpt:
-					#point.velocity = point.velocity.reverse()
-					point.velocity = physics.vector2d(0,0)
-					vect = vect.reverse()
-					#physics.applyForce(point,vect)
-				if dist <= 50 and not point.controlpt:
-					point.velocity = point.velocity.multiply(.5)
-		
-				if dist >= 200 and not point.controlpt:
+			#	if self.rect.colliderect(point.rect):
+			#		self.velocity = physics.vector2d(0,0)
+			#		point.velocity = physics.vector2d(0,0)
+
+				
+				#if dist <= 50:
+				#	point.velocity = point.velocity.multiply(.8)
+				"""
+				if dist >= 200:
 					a = random.randint(-70,70)
 					b = random.randint(-70,70)
 					if abs(a) > 30:
@@ -81,5 +108,37 @@ class controlPoint(blobPoint):
 					if abs(b) > 30:
 						b = random.randint(-70,70)
 					point.position = self.position.add(physics.vector2d(a,b))
-		# accelerate for gravity
+					"""			
+			for point in self.parts:#.controls:
+				
+				dist = math.sqrt(math.pow(self.position.x-point.position.x,2)+math.pow(self.position.y-point.position.y,2))
+				vect = physics.vector2d(self.position.x-point.position.x,self.position.y-point.position.y).unitize()
+				newdist = math.sqrt(math.pow(self.controls[0].position.x-point.position.x,2)+math.pow(self.controls[0].position.y-point.position.y,2))
+				
+				if dist > 20 and newdist > 20  and point.kind == "blobPoint" : #and newdist >  50:
+					#point.velocity = point.velocity.multiply(.5)
+					#vect = vect.divide(math.pow(dist,1.001))
+
+					physics.applyForce(point,vect.multiply(3))
+					#point.velocity = vect.multiply(5)
+					
+					#point.position = self.position
+					#point.position.y += random.choice((-1,1))/10
+					#point.position.x += random.choice((-1,1))/10
+					#point.velocity = vect.unitize().multiply(10)
+					#vect = vect.reverse()
+					#physics.applyForce(point,vect)
+				if dist < 2 and point.kind == "blobPoint" :#or newdist < 40:
+					#point.velocity = vect.unitize().multiply(-10)
+					physics.applyForce(point,vect.reverse().multiply(5))
+					#point.velocity = vect.multiply(-4)
+						
+					
+					
+
+	def bump(self,ptList):
+		#i = self.rect.collidelist(ptList)
+		#ptList[i].velocity = ptList[i].velocity.multiply(.8).reverse()	
+		return 0
+
 		
--- harvest_screen.py
+++ harvest_screen.py
@@ -3,10 +3,15 @@ import pygame, math, time
 # This is all the stuff for setting up the screen that the game is played on.
 # adjusting the size variable will alter the screen dimensions 
 
-size = width, height = 744,500
+#size = width, height = 744,500
+
 white = (255,255,255)
 pygame.init()
 pygame.font.init()
 speed = [1,1]
+background  = pygame.image.load("images/backGround1.bmp")
 
+backgroundRect = background.get_rect()
+size = (width, height) =background.get_size() 
 screen = pygame.display.set_mode(size)
+
